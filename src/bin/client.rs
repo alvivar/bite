0d2d03180f1265d3f3b1e1a467f4dc078ec6c066
Andrés Villalobos <andresalvivar@gmail.com>
2021-01-20 07:32:14 -0600

    Minimalist client that flushes weirdly (I don't get it yet), but works sending one message at the time, decent for testing

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,12 +1,18 @@
-use std::io::{BufWriter, Write};
+use std::io::{self, Write};
+use std::io::{BufRead, BufWriter};
 use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
-    let stream = TcpStream::connect("127.0.0.1:8888")?;
+    let stdin = io::stdin();
+    let stream = TcpStream::connect("127.0.0.1:1984")?;
     let mut writer = BufWriter::new(stream);
 
-    writer.write(b"      set          adros.eye0           baraka")?;
-    writer.flush()?;
+    for line in stdin.lock().lines() {
+        let content = line.unwrap();
+        writer.write(content.as_bytes()).unwrap();
+        writer.flush().unwrap();
+        break;
+    }
 
     Ok(())
 }
