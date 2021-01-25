fb57bb2129a1e6699fe5b45cee7ee74371ee0949
Andrés Villalobos <andresalvivar@gmail.com>
2021-01-24 21:17:51 -0600

    Client unique connection

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -3,12 +3,12 @@ use std::io::{BufRead, BufReader, BufWriter};
 use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
-    for line in io::stdin().lock().lines() {
-        let stream = TcpStream::connect("127.0.0.1:1984")?;
-        let stream_clone = stream.try_clone().unwrap();
-        let mut writer = BufWriter::new(stream);
-        let mut reader = BufReader::new(stream_clone);
+    let stream = TcpStream::connect("127.0.0.1:1984")?;
+    let stream_clone = stream.try_clone().unwrap();
+    let mut writer = BufWriter::new(stream);
+    let mut reader = BufReader::new(stream_clone);
 
+    for line in io::stdin().lock().lines() {
         let content = line.unwrap();
         writer.write(content.as_bytes()).unwrap();
         writer.write(&[0xA]).unwrap();
