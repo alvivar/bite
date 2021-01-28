df691160ea688abe003348854ca1bd0d28001516
Andrés Villalobos <andresalvivar@gmail.com>
2021-01-28 02:11:42 -0600

    Work in progress, new iteration

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,10 +1,10 @@
 use chrono::Utc;
+use std::collections::BTreeMap;
 use std::fs::OpenOptions;
 use std::io::{self, BufRead, BufReader, Read, Write};
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
-use std::{collections::BTreeMap, unimplemented};
 
 const DB_FILE: &str = "DB.json";
 const UTC_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
@@ -41,104 +41,9 @@ struct Client {
     addr: SocketAddr,
 }
 
-struct WorkerPool {
-    workers: Vec<Worker>,
-}
-
-impl WorkerPool {
-    pub fn new() -> WorkerPool {
-        WorkerPool {
-            workers: Vec::new(),
-        }
-    }
-
-    pub fn spawn(&mut self, worker: Worker) {
-        self.workers.push(worker);
-    }
-
-    pub fn command(&self, cmd: Command) {
-        // send a command to the worker threads
-    }
-
-    pub fn message(&self) -> Message {
-        // receive a message from the worker threads (blocking)
-        unimplemented!()
-    }
-}
-
-struct Worker {
-    handle: thread::JoinHandle<()>,
-    sender: mpsc::Sender<Command>,
-}
-
-impl Worker {
-    pub fn new(result: mpsc::Sender<Message>) -> Self {
-        let (sender, receiver) = mpsc::channel::<Command>();
-        let handle = thread::spawn(move || Self::worker(receiver, result));
-        Self { handle, sender }
-    }
-
-    pub fn command(&self, cmd: Command) {
-        self.sender.send(cmd).unwrap();
-    }
-
-    fn worker(command: mpsc::Receiver<Command>, result: mpsc::Sender<Message>) {
-        let mut clients = Vec::<Client>::new();
-
-        loop {
-            // Commands
-
-            match command.try_recv().unwrap() {
-                Command::New(socket, addr) => {
-                    println!("{} | {} | Connected", Utc::now().format(UTC_FORMAT), addr);
-                    clients.push(Client { socket, addr });
-                }
-                Command::End => break,
-            }
-
-            // Instructions parse
-
-            for i in 0..clients.len() {
-                // @ Non blocking read_line
-                let mut socket = clients[i].socket.try_clone().unwrap();
-                socket.set_nonblocking(true).unwrap();
-                let addr = clients[i].addr;
-
-                let mut reader = BufReader::new(&mut socket);
-                let mut content = String::new();
-                match reader.read_line(&mut content) {
-                    Ok(_) => {
-                        // @ TcpStream disconnection
-                        if content.len() <= 0 {
-                            clients.remove(i);
-                            println!(
-                                "{} | {} | Disconnected",
-                                Utc::now().format(UTC_FORMAT),
-                                addr
-                            );
-                            continue;
-                        }
-
-                        let process = parse_message(&mut content).unwrap();
-
-                        result
-                            .send(Message::Result(
-                                Client {
-                                    socket: socket.try_clone().unwrap(),
-                                    addr,
-                                },
-                                process,
-                            ))
-                            .unwrap();
-                    }
-                    Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {}
-                    Err(e) => {
-                        panic!("reader.read_line failed:\n{}", e);
-                    }
-                }
-            }
-        }
-    }
+enum MapMessage {
+    Get(String),
+    Set,
 }
 
 fn main() {
@@ -160,90 +65,45 @@ fn main() {
         *map = serde_json::from_str(&contents).unwrap();
     }
 
-    // New worker on incoming connections
+    // THREAD
+
+    // That returns information from the Map.
+
+    // THREAD
+
+    // That reads / writes data into the Map.
+
+    let (map_sender, map_receiver) = mpsc::channel::<MapMessage>();
+    thread::spawn(move || loop {
+        match map_receiver.recv() {
+            Ok(message) => match message {
+                Get(key) => match map.get(key) {
+                    Some(content) => Update {
+                        result: content.to_owned(),
+                    },
+                    None => Update {
+                        result: "".to_owned(),
+                    },
+                },
+                Set => {}
+            },
+            Err(e) => {
+                panic!("{}", e);
+            }
+        }
+    });
+
+    // MAIN THREAD
+
+    // That listens for new connections, and spawns Connection Handlers Threads.
 
     let server = TcpListener::bind("127.0.0.1:1984").unwrap();
     server.set_nonblocking(true).unwrap();
 
     let (sender, result) = mpsc::channel::<Message>();
-
-    // ?
-    let mut pool = WorkerPool::new();
-    pool.spawn(Worker::new(sender.clone()));
-    pool.spawn(Worker::new(sender.clone()));
-
-    let workers = [Worker::new(sender.clone()), Worker::new(sender.clone())];
-    let mut indx = 0;
-
-    thread::spawn(move || loop {
-        if let Ok((socket, addr)) = server.accept() {
-            workers[indx].command(Command::New(socket, addr));
-            indx = (indx + 1) % workers.len();
-        }
-    });
-
-    // The main thread processes the I/O, waiting from Workers messages.
-
     loop {
-        match result.recv() {
-            Ok(message) => {
-                match message {
-                    Message::Result(client, process) => {
-                        let mut socket = client.socket.try_clone().unwrap();
-
-                        // Map update
-
-                        let update = update_btreemap(process, data.clone());
-
-                        // The Client result
-
-                        socket.write(update.result.as_bytes()).unwrap();
-                        socket.write(&[0xA]).unwrap();
-                        socket.flush().unwrap();
-
-                        // Debug
-
-                        let inst = update.process.instruction;
-                        let mut val = update.process.value;
-
-                        let inst = match inst {
-                            Instruction::Get => {
-                                val = update.result;
-                                "GET"
-                            }
-                            Instruction::Set => {
-                                // Save to DBfile
-
-                                let map = data.lock().unwrap();
-                                let file = OpenOptions::new()
-                                    .read(true)
-                                    .write(true)
-                                    .create(true)
-                                    .truncate(true)
-                                    .open(DB_FILE);
-
-                                let json = serde_json::to_string(&*map).unwrap();
-                                file.unwrap().write_all(json.as_bytes()).unwrap();
-
-                                "SET"
-                            }
-                            Instruction::Nop => "NOP",
-                        };
-
-                        println!(
-                            "{} | {} | {} {} {}",
-                            Utc::now().format(UTC_FORMAT),
-                            client.addr,
-                            inst,
-                            update.process.key,
-                            val
-                        );
-                    }
-                }
-            }
-            Err(e) => {
-                panic!("receiver.recv() Failed:\n{}", e)
-            }
+        if let Ok((stream, addr)) = server.accept() {
+            // Spawn a thread than handles the Client connection
         }
     }
 }
@@ -324,3 +184,184 @@ fn update_btreemap(process: Process, data: Arc<Mutex<BTreeMap<String, String>>>)
         },
     }
 }
+
+// struct Worker {
+//     handle: thread::JoinHandle<()>,
+//     sender: mpsc::Sender<Command>,
+// }
+
+// impl Worker {
+//     pub fn new(result: mpsc::Sender<Message>) -> Self {
+//         let (sender, receiver) = mpsc::channel::<Command>();
+//         let handle = thread::spawn(move || Self::worker(receiver, result));
+//         Self { handle, sender }
+//     }
+
+//     pub fn command(&self, cmd: Command) {
+//         self.sender.send(cmd).unwrap();
+//     }
+
+//     fn worker(command: mpsc::Receiver<Command>, result: mpsc::Sender<Message>) {
+//         let mut clients = Vec::<Client>::new();
+
+//         loop {
+//             // Commands
+
+//             if let Ok(command) = command.try_recv() {
+//                 match command {
+//                     Command::New(socket, addr) => {
+//                         println!("{} | {} | Connected", Utc::now().format(UTC_FORMAT), addr);
+//                         clients.push(Client { socket, addr });
+//                     }
+//                     Command::End => break,
+//                 }
+//             }
+
+//             // Instructions parse
+
+//             for i in 0..clients.len() {
+//                 // @ Non blocking read_line
+//                 let mut socket = clients[i].socket.try_clone().unwrap();
+//                 socket.set_nonblocking(true).unwrap();
+//                 let addr = clients[i].addr;
+
+//                 let mut reader = BufReader::new(&mut socket);
+//                 let mut content = String::new();
+//                 match reader.read_line(&mut content) {
+//                     Ok(_) => {
+//                         // @ TcpStream disconnection
+//                         if content.len() <= 0 {
+//                             clients.remove(i);
+//                             println!(
+//                                 "{} | {} | Disconnected",
+//                                 Utc::now().format(UTC_FORMAT),
+//                                 addr
+//                             );
+//                             continue;
+//                         }
+
+//                         let process = parse_message(&mut content).unwrap();
+
+//                         result
+//                             .send(Message::Result(
+//                                 Client {
+//                                     socket: socket.try_clone().unwrap(),
+//                                     addr,
+//                                 },
+//                                 process,
+//                             ))
+//                             .unwrap();
+//                     }
+//                     Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {}
+//                     Err(e) => {
+//                         panic!("reader.read_line failed:\n{}", e);
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// struct WorkerPool {
+//     workers: Vec<Worker>,
+// }
+
+// impl WorkerPool {
+//     pub fn new() -> WorkerPool {
+//         WorkerPool {
+//             workers: Vec::new(),
+//         }
+//     }
+
+//     pub fn spawn(&mut self, worker: Worker) {
+//         self.workers.push(worker);
+//     }
+
+//     pub fn command(&self, cmd: Command) {
+//         // send a command to the worker threads
+//     }
+
+//     pub fn message(&self) -> Message {
+//         // receive a message from the worker threads (blocking)
+//         unimplemented!()
+//     }
+// }
+
+// let mut pool = WorkerPool::new();
+// pool.spawn(Worker::new(sender.clone()));
+// pool.spawn(Worker::new(sender.clone()));
+
+// let workers = [Worker::new(sender.clone()), Worker::new(sender.clone())];
+// let mut indx = 0;
+
+// thread::spawn(move || loop {
+//     if let Ok((socket, addr)) = server.accept() {
+//         workers[indx].command(Command::New(socket, addr));
+//         indx = (indx + 1) % workers.len();
+//     }
+// });
+
+// The main thread processes the I/O, waiting from Workers messages.
+
+// loop {
+//     match result.recv() {
+//         Ok(message) => {
+//             match message {
+//                 Message::Result(client, process) => {
+//                     let mut socket = client.socket.try_clone().unwrap();
+
+//                     // Map update
+
+//                     let update = update_btreemap(process, data.clone());
+
+//                     // The Client result
+
+//                     socket.write(update.result.as_bytes()).unwrap();
+//                     socket.write(&[0xA]).unwrap();
+//                     socket.flush().unwrap();
+
+//                     // Debug
+
+//                     let inst = update.process.instruction;
+//                     let mut val = update.process.value;
+
+//                     let inst = match inst {
+//                         Instruction::Get => {
+//                             val = update.result;
+//                             "GET"
+//                         }
+//                         Instruction::Set => {
+//                             // Save to DBfile
+
+//                             let map = data.lock().unwrap();
+//                             let file = OpenOptions::new()
+//                                 .read(true)
+//                                 .write(true)
+//                                 .create(true)
+//                                 .truncate(true)
+//                                 .open(DB_FILE);
+
+//                             let json = serde_json::to_string(&*map).unwrap();
+//                             file.unwrap().write_all(json.as_bytes()).unwrap();
+
+//                             "SET"
+//                         }
+//                         Instruction::Nop => "NOP",
+//                     };
+
+//                     println!(
+//                         "{} | {} | {} {} {}",
+//                         Utc::now().format(UTC_FORMAT),
+//                         client.addr,
+//                         inst,
+//                         update.process.key,
+//                         val
+//                     );
+//                 }
+//             }
+//         }
+//         Err(e) => {
+//             panic!("receiver.recv() Failed:\n{}", e)
+//         }
+//     }
+// }
