5ca45fb813dffb04031f11f29d2741f32ebe8d5e
Andrés Villalobos <andresalvivar@gmail.com>
2021-01-30 10:46:30 -0600

    Threads are created on execution demands, all clients are handled.

--- server/src/work.rs
+++ server/src/work.rs
@@ -1,7 +1,7 @@
-use std::sync::mpsc;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
+use std::{sync::mpsc, u32};
 
 trait FnBox {
     fn call_box(self: Box<Self>);
@@ -16,38 +16,57 @@ impl<F: FnOnce()> FnBox for F {
 type Job = Box<dyn FnBox + Send + 'static>;
 
 enum Message {
-    NewJob(Job),
+    NewJob(Job, Arc<Mutex<u32>>),
     Terminate,
 }
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
     sender: mpsc::Sender<Message>,
+    receiver: Arc<Mutex<mpsc::Receiver<Message>>>,
+    active_jobs: Arc<Mutex<u32>>,
 }
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let (sender, receiver) = mpsc::channel();
-
-        let receiver = Arc::new(Mutex::new(receiver));
-
         let mut workers = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let active_jobs = Arc::new(Mutex::new(0));
 
         for id in 0..size {
-            workers.push(Worker::new(id, Arc::clone(&receiver)));
+            workers.push(Worker::new(id, receiver.clone()));
         }
 
-        ThreadPool { workers, sender }
+        ThreadPool {
+            workers,
+            sender,
+            receiver,
+            active_jobs,
+        }
     }
 
-    pub fn execute<F>(&self, f: F)
+    pub fn execute<F>(&mut self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(Message::NewJob(job)).unwrap();
+
+        // New worker if the queue is busy.
+        let id = self.workers.len();
+        let workers_len = id as u32;
+
+        if *self.active_jobs.lock().unwrap() >= workers_len {
+            self.workers.push(Worker::new(id, self.receiver.clone()));
+            println!("Worker {} created.", id);
+        }
+
+        // Queue Job.
+        self.sender
+            .send(Message::NewJob(job, self.active_jobs.clone()))
+            .unwrap();
     }
 }
 
@@ -82,10 +101,14 @@ impl Worker {
             let message = receiver.lock().unwrap().recv().unwrap();
 
             match message {
-                Message::NewJob(job) => {
-                    println!("Worker {} executing.", id);
+                Message::NewJob(job, active_jobs) => {
+                    *active_jobs.lock().unwrap() += 1;
+                    println!("Worker {} executing a job.", id);
 
                     job.call_box();
+
+                    *active_jobs.lock().unwrap() -= 1;
+                    println!("Worker {} job ended.", id);
                 }
                 Message::Terminate => {
                     println!("Worker {} was told to terminate.", id);
