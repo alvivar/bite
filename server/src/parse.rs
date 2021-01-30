d09c277832e99813b3213d3186339fc078ac3ad9
Andrés Villalobos <andresalvivar@gmail.com>
2021-01-29 23:42:03 -0600

    To parse the Client message.

---
+++ server/src/parse.rs
@@ -0,0 +1,60 @@
+pub struct Process {
+    pub instruction: Instr,
+    pub key: String,
+    pub value: String,
+}
+
+pub enum Instr {
+    Set,
+    Get,
+    Nop,
+}
+
+pub fn from_message(content: &str) -> Process {
+    let mut inst = String::new();
+    let mut key = String::new();
+    let mut val = String::new();
+
+    let mut found = 0;
+    for c in content.trim().chars() {
+        match c {
+            ' ' => {
+                if val.len() > 0 {
+                    val.push(' ');
+                } else if key.len() > 0 {
+                    found = 2;
+                } else if inst.len() > 0 {
+                    found = 1;
+                }
+            }
+            _ => match found {
+                0 => {
+                    inst.push(c);
+                }
+                1 => {
+                    key.push(c);
+                }
+                _ => {
+                    val.push(c);
+                }
+            },
+        }
+    }
+
+    // println!("i[{}] k[{}] v[{}]", inst, key, val); // Debug
+
+    let instruction = match inst.trim().to_lowercase().as_str() {
+        "get" => Instr::Get,
+        "set" => Instr::Set,
+        _ => {
+            key = format!("{} {}", inst, key);
+            Instr::Nop
+        }
+    };
+
+    Process {
+        instruction,
+        key: key.trim().to_owned(),
+        value: val.trim().to_owned(),
+    }
+}
