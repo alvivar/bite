876cbe7298ac6b270daf876f81e09eb1a99b68b0
Andrés Villalobos <andresalvivar@gmail.com>
2021-04-07 23:55:11 -0600

    Delete?

--- server/src/main.rs
+++ server/src/main.rs
@@ -58,7 +58,7 @@ fn main() {
         let mut orphans = Vec::<usize>::new();
 
         for (i, mut s) in streams_lock.iter().enumerate() {
-            if let Err(e) = s.write(&[b'?']) {
+            if let Err(e) = s.write(&[0x7F]) {
                 orphans.push(i);
                 println!("Client error on ping: {}", e);
             }
