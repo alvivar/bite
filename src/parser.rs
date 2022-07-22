2296abc3f2ed64d39cdc54f3c57ecbfbe286cd61
Andrés Villalobos <andresalvivar@gmail.com>
2022-07-21 23:35:33 -0600

    Command is a better name.

--- src/parser.rs
+++ src/parser.rs
@@ -19,6 +19,37 @@ pub enum Cmd {
     Parse(usize, Vec<u8>, SocketAddr),
 }
 
+pub struct Message {
+    pub command: Command,
+    pub key: String,
+    pub value: String,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum Command {
+    Nop,
+    Set,
+    SetIfNone,
+    Inc,
+    Append,
+    Delete,
+    Get,
+    KeyValue,
+    Jtrim,
+    Json,
+    SubGet,
+    SubKeyValue,
+    SubJson,
+    Unsub,
+    SubCall,
+}
+
+impl Display for Command {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        Debug::fmt(self, f)
+    }
+}
+
 pub struct Parser {
     pub tx: Sender<Cmd>,
     rx: Receiver<Cmd>,
@@ -39,30 +70,30 @@ impl Parser {
     ) {
         loop {
             match self.rx.recv().unwrap() {
-                Cmd::Parse(id, message, addr) => {
-                    // We assume multiple instructions separated with newlines.
-                    let mut cursor = Cursor::new(&message[..]);
+                Cmd::Parse(id, data, addr) => {
+                    // We assume multiple commands separated with newlines.
+                    let mut cursor = Cursor::new(&data[..]);
                     while let Some(line) = next_line(&mut cursor) {
-                        let msg = parse(line);
-                        let instr = msg.instr;
-                        let key = msg.key;
-                        let value = msg.value;
+                        let message = parse(line);
+                        let command = message.command;
+                        let key = message.key;
+                        let value = message.value;
 
-                        println!("\n{}: {} {} {}", addr, instr, key, value);
+                        println!("\n{}: {} {} {}", addr, command, key, value);
 
-                        match instr {
-                            // Instructions that doesn't make sense without key.
-                            _ if key.is_empty() && needs_key(&instr) => {
+                        match command {
+                            // Commands that doesn't make sense without key.
+                            _ if key.is_empty() && needs_key(&command) => {
                                 writer_tx.send(Push(id, KEY.into())).unwrap();
                             }
 
                             // Nop
-                            Instr::Nop => {
+                            Command::Nop => {
                                 writer_tx.send(Push(id, NOP.into())).unwrap();
                             }
 
                             // Set
-                            Instr::Set => {
+                            Command::Set => {
                                 subs_tx
                                     .send(Call(key.to_owned(), value.to_owned()))
                                     .unwrap();
@@ -72,57 +103,51 @@ impl Parser {
                             }
 
                             // Set only if the key doesn't exists.
-                            Instr::SetIfNone => {
+                            Command::SetIfNone => {
                                 data_tx.send(SetIfNone(key, value)).unwrap();
                                 writer_tx.send(Push(id, OK.into())).unwrap();
                             }
 
                             // Makes the value an integer and increase it in 1.
-                            Instr::Inc => {
+                            Command::Inc => {
                                 data_tx.send(Inc(key, id)).unwrap();
                             }
 
                             // Appends the value.
-                            Instr::Append => {
+                            Command::Append => {
                                 data_tx.send(Append(key, value, id)).unwrap();
                             }
 
                             // Delete!
-                            Instr::Delete => {
+                            Command::Delete => {
                                 data_tx.send(Delete(key)).unwrap();
                                 writer_tx.send(Push(id, OK.into())).unwrap();
                             }
 
                             // Get
-                            Instr::Get => {
+                            Command::Get => {
                                 data_tx.send(Get(key, id)).unwrap();
                             }
 
                             // 0x0 separated key value enumeration: key value\0x0key2 value2
-                            Instr::KeyValue => {
+                            Command::KeyValue => {
                                 data_tx.send(Bite(key, id)).unwrap();
                             }
 
                             // Trimmed Json (just the data).
-                            Instr::Jtrim => {
+                            Command::Jtrim => {
                                 data_tx.send(Jtrim(key, id)).unwrap();
                             }
 
                             // Json (full path).
-                            Instr::Json => {
+                            Command::Json => {
                                 data_tx.send(Json(key, id)).unwrap();
                             }
 
                             // A generic "bite" subscription. Subscribers also receive their key: "key value"
                             // Also a first message if value is available.
-                            Instr::SubGet | Instr::SubKeyValue | Instr::SubJson => {
-                                // @todo This validation below should happen, probably is subs_tx responsability.
-
-                                // if !keys.contains(&key) {
-                                //     keys.push(key.to_owned());
-                                // }
-
-                                subs_tx.send(Add(key.to_owned(), id, instr)).unwrap();
+                            Command::SubGet | Command::SubKeyValue | Command::SubJson => {
+                                subs_tx.send(Add(key.to_owned(), id, command)).unwrap();
 
                                 if !value.is_empty() {
                                     subs_tx.send(Call(key, value)).unwrap()
@@ -132,7 +157,7 @@ impl Parser {
                             }
 
                             // A unsubscription and a last message if value is available.
-                            Instr::Unsub => {
+                            Command::Unsub => {
                                 if !value.is_empty() {
                                     subs_tx.send(Call(key.to_owned(), value)).unwrap();
                                 }
@@ -142,7 +167,7 @@ impl Parser {
                             }
 
                             // Calls key subscribers with the new value without data modifications.
-                            Instr::SubCall => {
+                            Command::SubCall => {
                                 subs_tx.send(Call(key, value)).unwrap();
                                 writer_tx.send(Push(id, OK.into())).unwrap();
                             }
@@ -154,89 +179,62 @@ impl Parser {
     }
 }
 
-pub struct Msg {
-    pub instr: Instr,
-    pub key: String,
-    pub value: String,
-}
-
-#[derive(PartialEq, Debug)]
-pub enum Instr {
-    Nop,
-    Set,
-    SetIfNone,
-    Inc,
-    Append,
-    Delete,
-    Get,
-    KeyValue,
-    Jtrim,
-    Json,
-    SubGet,
-    SubKeyValue,
-    SubJson,
-    Unsub,
-    SubCall,
-}
-
-impl Display for Instr {
-    fn fmt(&self, f: &mut Formatter) -> Result {
-        Debug::fmt(self, f)
-    }
-}
-
-/// Returns a Msg with the first character found as instruction,
+/// Returns a Msg with the first character found as command,
 /// the next word as key, and the rest as value.
 
-/// This text: +hello world is a pretty old meme
-/// Returns: Msg { Instr::Append, "hello", "world is a pretty old meme" }
+/// This text: + hello world is a pretty old meme
+/// Returns: Msg { Command::Append, "hello", "world is a pretty old meme" }
 
-pub fn parse(msg: &[u8]) -> Msg {
+pub fn parse(msg: &[u8]) -> Message {
     let mut cursor = Cursor::new(msg);
     let op = to_utf8(next_word(&mut cursor));
     let key = to_utf8(next_word(&mut cursor));
     let value = from_utf8(remaining(&mut cursor)).unwrap();
 
-    let instr = match op.to_lowercase().trim_end() {
-        "s" => Instr::Set,
-        "s?" => Instr::SetIfNone,
-        "+1" => Instr::Inc,
-        "+" => Instr::Append,
-        "d" => Instr::Delete,
-        "g" => Instr::Get,
-        "k" => Instr::KeyValue,
-        "j" => Instr::Jtrim,
-        "js" => Instr::Json,
-        "#g" => Instr::SubGet,
-        "#k" => Instr::SubKeyValue,
-        "#j" => Instr::SubJson,
-        "#-" => Instr::Unsub,
-        "!" => Instr::SubCall,
-        _ => Instr::Nop,
+    let command = match op.to_lowercase().trim_end() {
+        "s" => Command::Set,
+        "s?" => Command::SetIfNone,
+        "+1" => Command::Inc,
+        "+" => Command::Append,
+        "d" => Command::Delete,
+        "g" => Command::Get,
+        "k" => Command::KeyValue,
+        "j" => Command::Jtrim,
+        "js" => Command::Json,
+        "#g" => Command::SubGet,
+        "#k" => Command::SubKeyValue,
+        "#j" => Command::SubJson,
+        "#-" => Command::Unsub,
+        "!" => Command::SubCall,
+        _ => Command::Nop,
     };
 
     // @todo In the future value needs to be a [u8] or at least a Vec<u8>.
     let key = key.trim_end().to_owned();
     let value = value.trim_end().to_owned();
 
-    Msg { instr, key, value }
+    Message {
+        command,
+        key,
+        value,
+    }
 }
 
-pub fn needs_key(instr: &Instr) -> bool {
-    match instr {
-        Instr::Nop | Instr::KeyValue | Instr::Jtrim | Instr::Json => false,
+pub fn needs_key(command: &Command) -> bool {
+    match command {
+        Command::Nop | Command::KeyValue | Command::Jtrim | Command::Json => false,
 
-        Instr::Set
-        | Instr::SetIfNone
-        | Instr::Inc
-        | Instr::Append
-        | Instr::Delete
-        | Instr::Get
-        | Instr::SubGet
-        | Instr::SubKeyValue
-        | Instr::SubJson
-        | Instr::Unsub
-        | Instr::SubCall => true,
+        Command::Set
+        | Command::SetIfNone
+        | Command::Inc
+        | Command::Append
+        | Command::Delete
+        | Command::Get
+        | Command::SubGet
+        | Command::SubKeyValue
+        | Command::SubJson
+        | Command::Unsub
+        | Command::SubCall => true,
     }
 }
 
