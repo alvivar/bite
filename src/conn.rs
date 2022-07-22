ab72abeedb0a1044b0e171bb9cd67e222a21e692
Andrés Villalobos <andresalvivar@gmail.com>
2022-07-21 23:12:35 -0600

    Clean up

--- src/conn.rs
+++ src/conn.rs
@@ -6,21 +6,18 @@ pub struct Connection {
     pub id: usize,
     pub socket: TcpStream,
     pub addr: SocketAddr,
-    pub keys: Vec<String>, // Only reading connections end up having subscription keys.
     pub to_send: Vec<String>,
     pub closed: bool,
 }
 
 impl Connection {
     pub fn new(id: usize, socket: TcpStream, addr: SocketAddr) -> Connection {
-        let keys = Vec::<String>::new();
         let to_send = Vec::<String>::new();
 
         Connection {
             id,
             socket,
             addr,
-            keys,
             to_send,
             closed: false,
         }
