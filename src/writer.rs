ab93f3062407e61b8e9c7cb208b58903d5a19642
Andrés Villalobos <andresalvivar@gmail.com>
2022-07-26 23:10:39 -0600

    Also here.

--- src/writer.rs
+++ src/writer.rs
@@ -48,6 +48,9 @@ impl Writer {
             match self.rx.recv().unwrap() {
                 Cmd::Push(id, msg) => {
                     if let Some(conn) = self.writers.lock().unwrap().get_mut(&id) {
+                        // @todo Wondering if triming the string affects certain
+                        // type of messages? Or should be part of the protocol?
+
                         let mut msg = msg.trim_end().to_owned();
                         msg.push('\n');
                         conn.to_send.push(msg);
