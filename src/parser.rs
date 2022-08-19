f878a81e8050b5deb556d3e5e996ce8be5c1c3c0
Andrés Villalobos <andresalvivar@gmail.com>
2022-08-19 03:09:01 -0600

    One instruction at the time.

--- src/parser.rs
+++ src/parser.rs
@@ -70,113 +70,110 @@ impl Parser {
             match self.rx.recv().unwrap() {
                 Cmd::Parse(id, data, addr) => {
                     // We assume multiple commands separated with newlines.
-                    let mut cursor = Cursor::new(&data[..]);
-                    while let Some(line) = next_line(&mut cursor) {
-                        let utf8 = String::from_utf8_lossy(line);
-                        let mut text = utf8.to_string();
+                    let utf8 = String::from_utf8_lossy(&data);
+                    let mut text = utf8.to_string();
 
-                        let max = 1024;
-                        if utf8.len() > max {
-                            text = truncate(&utf8, max).into();
-                            let add = format!("[..{}]", max);
-                            text.push_str(&add);
-                        };
+                    let max = 1024;
+                    if utf8.len() > max {
+                        text = truncate(&utf8, max).into();
+                        let add = format!("[..{}]", max);
+                        text.push_str(&add);
+                    };
 
-                        println!("\n{} ({} bytes): {}", addr, line.len(), text);
+                    println!("\n{} ({} bytes): {}", addr, data.len(), text);
 
-                        let message = parse(line);
-                        let command = message.command;
-                        let key = message.key;
-                        let data = message.data;
+                    let message = parse(&data);
+                    let command = message.command;
+                    let key = message.key;
+                    let data = message.data;
 
-                        match command {
-                            // Commands that doesn't make sense without key.
-                            _ if key.is_empty() && needs_key(&command) => {
-                                writer_tx.send(Queue(id, NO.into())).unwrap();
+                    match command {
+                        // Commands that doesn't make sense without key.
+                        _ if key.is_empty() && needs_key(&command) => {
+                            writer_tx.send(Queue(id, NO.into())).unwrap();
+                        }
+
+                        // Nop
+                        Command::Nop => {
+                            writer_tx.send(Queue(id, NO.into())).unwrap();
+                        }
+
+                        // Set
+                        Command::Set => {
+                            writer_tx.send(Queue(id, OK.into())).unwrap();
+                            subs_tx.send(Call(key.to_owned(), data.to_owned())).unwrap();
+                            data_tx.send(Set(key, data)).unwrap();
+                        }
+
+                        // Set only if the key doesn't exists.
+                        Command::SetIfNone => {
+                            writer_tx.send(Queue(id, OK.into())).unwrap();
+                            data_tx.send(SetIfNone(key, data)).unwrap();
+                        }
+
+                        // Makes the value an integer and increase it in 1.
+                        Command::Inc => {
+                            data_tx.send(Inc(key, id)).unwrap();
+                        }
+
+                        // Appends the value.
+                        Command::Append => {
+                            data_tx.send(Append(key, data, id)).unwrap();
+                        }
+
+                        // Delete!
+                        Command::Delete => {
+                            writer_tx.send(Queue(id, OK.into())).unwrap();
+                            data_tx.send(Delete(key)).unwrap();
+                        }
+
+                        // Get
+                        Command::Get => {
+                            data_tx.send(Get(key, id)).unwrap();
+                        }
+
+                        // 0x0 separated key value enumeration: key value\0x0key2 value2
+                        Command::KeyValue => {
+                            data_tx.send(KeyValue(key, id)).unwrap();
+                        }
+
+                        // Trimmed Json (just the data).
+                        Command::Jtrim => {
+                            data_tx.send(Jtrim(key, id)).unwrap();
+                        }
+
+                        // Json (full path).
+                        Command::Json => {
+                            data_tx.send(Json(key, id)).unwrap();
+                        }
+
+                        // A generic "bite" subscription. Subscribers also receive their key: "key value"
+                        // Also a first message if value is available.
+                        Command::SubGet | Command::SubKeyValue | Command::SubJson => {
+                            writer_tx.send(Queue(id, OK.into())).unwrap();
+
+                            subs_tx.send(Add(key.to_owned(), id, command)).unwrap();
+
+                            if !data.is_empty() {
+                                subs_tx.send(Call(key, data)).unwrap()
+                            }
+                        }
+
+                        // A unsubscription and a last message if value is available.
+                        Command::Unsub => {
+                            writer_tx.send(Queue(id, OK.into())).unwrap();
+
+                            if !data.is_empty() {
+                                subs_tx.send(Call(key.to_owned(), data)).unwrap();
                             }
 
-                            // Nop
-                            Command::Nop => {
-                                writer_tx.send(Queue(id, NO.into())).unwrap();
-                            }
+                            subs_tx.send(Del(key, id)).unwrap();
+                        }
 
-                            // Set
-                            Command::Set => {
-                                writer_tx.send(Queue(id, OK.into())).unwrap();
-                                subs_tx.send(Call(key.to_owned(), data.to_owned())).unwrap();
-                                data_tx.send(Set(key, data)).unwrap();
-                            }
-
-                            // Set only if the key doesn't exists.
-                            Command::SetIfNone => {
-                                writer_tx.send(Queue(id, OK.into())).unwrap();
-                                data_tx.send(SetIfNone(key, data)).unwrap();
-                            }
-
-                            // Makes the value an integer and increase it in 1.
-                            Command::Inc => {
-                                data_tx.send(Inc(key, id)).unwrap();
-                            }
-
-                            // Appends the value.
-                            Command::Append => {
-                                data_tx.send(Append(key, data, id)).unwrap();
-                            }
-
-                            // Delete!
-                            Command::Delete => {
-                                writer_tx.send(Queue(id, OK.into())).unwrap();
-                                data_tx.send(Delete(key)).unwrap();
-                            }
-
-                            // Get
-                            Command::Get => {
-                                data_tx.send(Get(key, id)).unwrap();
-                            }
-
-                            // 0x0 separated key value enumeration: key value\0x0key2 value2
-                            Command::KeyValue => {
-                                data_tx.send(KeyValue(key, id)).unwrap();
-                            }
-
-                            // Trimmed Json (just the data).
-                            Command::Jtrim => {
-                                data_tx.send(Jtrim(key, id)).unwrap();
-                            }
-
-                            // Json (full path).
-                            Command::Json => {
-                                data_tx.send(Json(key, id)).unwrap();
-                            }
-
-                            // A generic "bite" subscription. Subscribers also receive their key: "key value"
-                            // Also a first message if value is available.
-                            Command::SubGet | Command::SubKeyValue | Command::SubJson => {
-                                writer_tx.send(Queue(id, OK.into())).unwrap();
-
-                                subs_tx.send(Add(key.to_owned(), id, command)).unwrap();
-
-                                if !data.is_empty() {
-                                    subs_tx.send(Call(key, data)).unwrap()
-                                }
-                            }
-
-                            // A unsubscription and a last message if value is available.
-                            Command::Unsub => {
-                                writer_tx.send(Queue(id, OK.into())).unwrap();
-
-                                if !data.is_empty() {
-                                    subs_tx.send(Call(key.to_owned(), data)).unwrap();
-                                }
-
-                                subs_tx.send(Del(key, id)).unwrap();
-                            }
-
-                            // Calls key subscribers with the new value without data modifications.
-                            Command::SubCall => {
-                                writer_tx.send(Queue(id, OK.into())).unwrap();
-                                subs_tx.send(Call(key, data)).unwrap();
-                            }
+                        // Calls key subscribers with the new value without data modifications.
+                        Command::SubCall => {
+                            writer_tx.send(Queue(id, OK.into())).unwrap();
+                            subs_tx.send(Call(key, data)).unwrap();
                         }
                     }
                 }
@@ -242,6 +239,7 @@ pub fn needs_key(command: &Command) -> bool {
     }
 }
 
+#[allow(dead_code)]
 pub fn next_line<'a>(cursor: &mut Cursor<&'a [u8]>) -> Option<&'a [u8]> {
     let mut start = cursor.position() as usize;
     let mut end = cursor.get_ref().len();
