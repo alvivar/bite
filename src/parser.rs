3970472aef6e491e1acab8dfaa28bdad93dee28e
Andrés Villalobos <andresalvivar@gmail.com>
2022-08-19 02:52:17 -0600

    Simpler.

--- src/parser.rs
+++ src/parser.rs
@@ -328,6 +328,6 @@ fn is_space(c: u8) -> bool {
 fn truncate(s: &str, max_chars: usize) -> &str {
     match s.char_indices().nth(max_chars) {
         None => s,
-        Some((idx, _)) => &s[..idx],
+        Some((i, _)) => &s[..i],
     }
 }
