9a11a17a76513daf62b6e2230d771f1069c923c7
Andrés Villalobos <andresalvivar@gmail.com>
2022-09-06 01:21:31 -0600

    The analytics does't work yet, but this is an stable client for the new protocol (check protocol.md). Pretty awesome honestly. Still needs to deal with responses related by ID to a callback.

--- .csharp/Bitf.cs
+++ .csharp/Bitf.cs
@@ -1,33 +1,61 @@
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
 
+/// Utilities to cast bytes.
 public static class Bitf
 {
-    public static int Int(string str, int or)
+    public static int Int(string str, int or = 0)
     {
         int n;
         return int.TryParse(str, NumberStyles.Any, CultureInfo.InvariantCulture, out n) ? n : or;
     }
 
-    public static float Float(string str, float or)
+    public static int Int(byte[] bigEndian, int or = 0)
+    {
+        if (bigEndian.Length < 4)
+            return or;
+
+        bigEndian = SubArray(bigEndian, 0, 4);
+
+        if (BitConverter.IsLittleEndian)
+            Array.Reverse(bigEndian);
+
+        return BitConverter.ToInt32(bigEndian, 0);
+    }
+
+    public static float Float(string str, float or = 0)
     {
         float n;
         return float.TryParse(str, NumberStyles.Any, CultureInfo.InvariantCulture, out n) ? n : or;
     }
 
-    public static long Long(string str, long or)
+    public static long Long(string str, long or = 0)
     {
         long n;
         return long.TryParse(str, NumberStyles.Any, CultureInfo.InvariantCulture, out n) ? n : or;
     }
 
+    public static long Long(byte[] bigEndian, long or = 0)
+    {
+        if (bigEndian.Length < 8)
+            return or;
+
+        bigEndian = SubArray(bigEndian, 0, 8);
+
+        if (BitConverter.IsLittleEndian)
+            Array.Reverse(bigEndian);
+
+        return BitConverter.ToInt64(bigEndian, 0);
+    }
+
     public static string Str(float f, int precision)
     {
-        precision = (int) Mathf.Pow(10, precision);
+        precision = (int)Mathf.Pow(10, precision);
 
-        var intf = (int) f;
-        var decf = (int) Mathf.Abs((f - intf) * precision);
+        var intf = (int)f;
+        var decf = (int)Mathf.Abs((f - intf) * precision);
 
         if (decf == 0)
             return $"{intf}";
@@ -35,9 +63,14 @@ public static class Bitf
         return $"{intf}.{decf}";
     }
 
+    public static string Str(byte[] f)
+    {
+        return System.Text.Encoding.UTF8.GetString(f, 0, f.Length);
+    }
+
     public static float Round(float f, int precision)
     {
-        precision = (int) Mathf.Pow(10, precision);
+        precision = (int)Mathf.Pow(10, precision);
 
         return Mathf.Round(f * precision) / precision;
     }
@@ -54,5 +87,13 @@ public static class Bitf
         }
 
         return numbers.ToArray();
+    }
+
+    private static T[] SubArray<T>(this T[] data, int index, int length)
+    {
+        T[] result = new T[length];
+        Array.Copy(data, index, result, 0, length);
+
+        return result;
     }
 }
--- .csharp/README.md
+++ .csharp/README.md
@@ -1,6 +1,7 @@
-# C# BITE
+# C# Bite
 
-C# client library for [**Bite**](https://github.com/alvivar/bite). Compatible with Unity.
+C# client library for [**Bite**](https://github.com/alvivar/bite). Unity
+compatible.
 
 To **connect**.
 
@@ -11,19 +12,22 @@ To **send**.
     bite.Send("s author Andrés Villalobos");
     bite.Send("g author");
 
-You can use a **System.Action<string>** callback on **Send** to deal directly with the **response**.
+You can use a **System.Action<Byte[]>** callback on **Send** to deal directly
+with the **response**.
 
     bite.Send("g author", response => {
         // Handle your response.
     });
 
-You also have a couple **System.Action<string>** to subscribe.
+You also have a **System.Action<Byte[]>** to subscribe.
 
     bite.OnResponse += YourOnResponse;
-    bite.OnError += YourOnError;
 
-Bonus: There are some static utility to deal with string to ints, floats or longs.
+Bonus: There are some static utility to deal with string to ints, floats or
+longs.
 
-    int i = Bitf.Int("Try or default ->", 1984);
+    int i = Bite.Int("Try or default ->", 1984);
 
-Check out the **Unity** [**Analytics.cs**](https://github.com/alvivar/bite/blob/master/.csharp/Analytics.cs) example!
+Check out the **Unity**
+[**Analytics.cs**](https://github.com/alvivar/bite/blob/master/.csharp/Analytics.cs)
+example!
